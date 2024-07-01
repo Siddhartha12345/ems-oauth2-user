a6bbf60db2b3af599d84a6cae5be3f4770695e3c
Siddhartha <siddharthapaul101@gmail.com>
2024-07-01 22:33:41 +0530

    initial commit

---
+++ src/main/java/com/oauth2/EmsOauth2UserApplication.java
@@ -0,0 +1,13 @@
+package com.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsOauth2UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsOauth2UserApplication.class, args);
+	}
+
+}
